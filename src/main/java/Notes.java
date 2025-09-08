e6d908bfe7c1ce06611251d8c9d2e4f9d7fe7dda
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2025-09-08 18:34:08 +0000

    update notes

--- src/main/java/Notes.java
+++ src/main/java/Notes.java
@@ -1,6 +1,39 @@
+import java.util.Scanner;
 
 class Notes {
 	public static void main(String[] args) {
 
+		//input
+		Scanner s = new Scanner(System.in);
+
+		//each save as their appropriate
+		// s.nextLine(); //String
+		// s.nextInt(); //Int
+		// s.nextDouble(); //double
+		// s.nextBoolean(); //boolean
+		// s.next(); //don't use
+
+
+		// int a = s.nextInt();
+		// System.out.println(a+10);
+		
+		
+		//DONT DO THIS
+		// double a = s.nextDouble();
+		// System.out.println(a);
+		// String b = s.nextLine();
+		// System.out.println(b);
+
+		//clear the scanner;
+		//nextInt, nextDouble, nextBoolean (ANYTHING BUT NEXTLINE)
+		//clear the scanner using nextLine();
+		// int a = s.nextInt();
+		// s.nextLine(); //clears the scanner;
+
+		//prompt
+
+		System.out.print("Input a number: ");
+		int a = s.nextInt();
+		System.out.println("Your number was " + a);
 	}
 }
